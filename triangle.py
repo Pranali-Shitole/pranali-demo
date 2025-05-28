5311ce28e4c7f8bcdecebaf914559ee4e1a27d94
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-05-28 21:16:00 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,4 @@
+base = float(input("Enter the length of the base of the triangle: "))
+height = float(input("Enter the heidht of the triangle: "))
+area = 0.5 * base * height
+print(f"the area of the triangle: {area}")
