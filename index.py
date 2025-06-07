17f760ff2e37b5a11903c55e35ec3d6e03ab37e6
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-06-07 20:02:07 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,7 @@
+def index_of_caps(word):
+ return [i for i, char in enumerate(word) if char.isupper()]
+index_of_caps("eDaBiT")
+index_of_caps("determine")
+index_of_caps("STRIKE")
+index_of_caps("eQuINoX") 
+index_of_caps("sUn")
