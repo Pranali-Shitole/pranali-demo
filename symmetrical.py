128c315bfffc2034e5db8e1799c42f7968d7f085
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-06-09 19:32:24 +0530

    Add files via upload

---
+++ symmetrical.py
@@ -0,0 +1,8 @@
+def is_symmetrical(num):
+ num_str = str(num)
+ return num_str == num_str[::-1]
+is_symmetrical(7227)
+is_symmetrical(12567) 
+is_symmetrical(44444444)
+is_symmetrical(44444444)
+is_symmetrical(1112111)
