3f5c9e60be467792807e3d5253eb7f8c6657cce9
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-06-10 20:56:13 +0530

    Add files via upload

---
+++ sort by length.py
@@ -0,0 +1,5 @@
+def sort_by_length(lst):
+ return sorted(lst, key=len)  
+sort_by_length(["Google", "Apple", "Microsoft"]) 
+sort_by_length(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+sort_by_length(["Turing", "Einstein", "Jung"])
