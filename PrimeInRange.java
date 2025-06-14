db5ac60f729a0e1e78f9ad9e2d6b871b71d866fa
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-06-14 19:09:49 +0530

    Add files via upload

---
+++ PrimeInRange.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PrimeInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter ending number: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
+
