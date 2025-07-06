ee3f9d6590dc8538853dae211dfa24c2fff8f65e
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-07-06 19:41:08 +0530

    Add files via upload

---
+++ SimpleWebSocketServer.java
@@ -0,0 +1,46 @@
+import org.java_websocket.server.WebSocketServer;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+
+import java.net.InetSocketAddress;
+
+public class SimpleWebSocketServer extends WebSocketServer {
+
+    public SimpleWebSocketServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        System.out.println("New connection from " + conn.getRemoteSocketAddress());
+        conn.send("Hello from the server!"); 
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("Connection closed: " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("Received: " + message);
+        conn.send("Echo: " + message);
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("WebSocket server started on port " + getPort());
+    }
+
+    public static void main(String[] args) {
+        int port = 8887; 
+        SimpleWebSocketServer server = new SimpleWebSocketServer(new InetSocketAddress(port));
+        server.start();
+    }
+}
+
