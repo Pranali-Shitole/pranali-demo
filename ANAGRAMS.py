95c2c889a8fd3547b7ca30c2f51868961275eec3
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-07-16 19:43:58 +0530

    Add files via upload

---
+++ ANAGRAMS.py
@@ -0,0 +1,8 @@
+str1 = "listen"
+str2 = "silent"
+str1 = str1.replace("","").upper()
+str2 = str2.replace("","").upper()
+if sorted(str1) == sorted(str2):
+    print("true")
+else:
+    print("false")
