9636673d85a90ac11e1d92487bf1e9385bd6c0f8
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-07-17 19:40:33 +0530

    Add files via upload

---
+++ countingspecialcharacter.py
@@ -0,0 +1,10 @@
+def count_sp_char(string):
+    sp_char = "!@#$%^&*><?:~~"
+    count = 0
+    for i in strin:
+        i in sp_char :
+        count+=1
+        return count
+    text = 'hello! how are you? #specialcharacter!123'
+    result = count_sp_char(text)
+    print(count)
