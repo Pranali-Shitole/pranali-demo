228560426876981499d5222fc749af45655e1d2a
Pranali-Shitole <pranalishitole9028@gmail.com>
2025-07-20 22:02:51 +0530

    Add files via upload

---
+++ splitfunction.py
@@ -0,0 +1,15 @@
+text = "apple,banana,cherry"
+fruits = text.split(",")
+print(fruits)  
+sentence = "Python is powerful"
+words = sentence.split()
+print(words)  
+words = ['apple', 'banana', 'cherry']
+text = ", ".join(words)
+print(text) 
+chars = ['P', 'y', 't', 'h', 'o', 'n']
+word = "".join(chars)
+print(word) 
+
+
+
